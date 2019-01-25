c33218f09d43a1f7d9cd4c54b637b542e6b974d9
kani <must90@gmail.com>
2019-01-25 17:14:07 +0530

    Update data.sql

--- data.sql
+++ data.sql
@@ -2,3 +2,4 @@ insert into `employee_detail` (`employee_name`,`employee_reporting_name`,`employ
 insert into `employee_detail` (`employee_name`,`employee_reporting_name`,`employee_contact_no`) values ('logan','abdul','1234567890');
 insert into `employee_detail` (`employee_name`,`employee_reporting_name`,`employee_contact_no`) values ('selva','abdul','1234567890');
 insert into `employee_detail` (`employee_name`,`employee_reporting_name`,`employee_contact_no`) values ('ramya','abdul','1234567890');
+insert into `employee_detail` (`employee_name`,`employee_reporting_name`,`employee_contact_no`) values ('sathya','abdul','1234567890');
