34711306566f5b6b168f8361de96979f01603073
kani <must90@gmail.com>
2019-01-25 16:13:00 +0530

    Create data.sql
    added file

---
+++ data.sql
@@ -0,0 +1,5 @@
+insert into `employee_detail` (`employee_name`,`employee_reporting_name`,`employee_contact_no`) values ('sathya','abdul','1234567890');
+insert into `employee_detail` (`employee_name`,`employee_reporting_name`,`employee_contact_no`) values ('kani','abdul','1234567890');
+insert into `employee_detail` (`employee_name`,`employee_reporting_name`,`employee_contact_no`) values ('logan','abdul','1234567890');
+insert into `employee_detail` (`employee_name`,`employee_reporting_name`,`employee_contact_no`) values ('selva','abdul','1234567890');
+insert into `employee_detail` (`employee_name`,`employee_reporting_name`,`employee_contact_no`) values ('ramya','abdul','1234567890');
