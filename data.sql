7422e8f4781ab08d3d44aa4abd3bf8feb4475fd8
kani <must90@gmail.com>
2019-01-25 17:01:33 +0530

    Update data.sql
    removed 1 row

--- data.sql
+++ data.sql
@@ -3,4 +3,3 @@ insert into `employee_detail` (`employee_name`,`employee_reporting_name`,`employ
 insert into `employee_detail` (`employee_name`,`employee_reporting_name`,`employee_contact_no`) values ('logan','abdul','1234567890');
 insert into `employee_detail` (`employee_name`,`employee_reporting_name`,`employee_contact_no`) values ('selva','abdul','1234567890');
 insert into `employee_detail` (`employee_name`,`employee_reporting_name`,`employee_contact_no`) values ('ramya','abdul','1234567890');
-insert into `employee_detail` (`employee_name`,`employee_reporting_name`,`employee_contact_no`) values ('test','abdul','1234567890');
